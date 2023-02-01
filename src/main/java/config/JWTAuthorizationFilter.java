5f82604e4429bf6f707fec8e0f7f977ea366e889
maniac787 <maniac787@gmail.com>
2023-01-31 22:58:59 -0500

    perf: refactor packages

--- src/main/java/config/JWTAuthorizationFilter.java
+++ src/main/java/config/JWTAuthorizationFilter.java
@@ -1,4 +1,4 @@
-package inicio;
+package config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
--- src/main/java/inicio/Application.java
+++ src/main/java/inicio/Application.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
-@ComponentScan(basePackages = {"controller", "repository", "service", "inicio"})
+@ComponentScan(basePackages = {"controller", "repository", "service", "inicio", "config"})
 @EntityScan(basePackages = {"model"})
 @EnableJpaRepositories(basePackages = {"repository"})
 @SpringBootApplication
